5adc8d712ba324c49192cdb1115e42f903f0ddff
joebiden42 <46309470+joebiden42@users.noreply.github.com>
2019-01-02 00:45:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json")
 
 client.on("ready", () => {
 	client.user.setActivity(`Bow. (wow)`);
