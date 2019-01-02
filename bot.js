02083e46aba699ce6f754d174c3c819775ab03a6
joebiden42 <46309470+joebiden42@users.noreply.github.com>
2019-01-02 00:30:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json")
 
 client.on("ready", () => {
-	console.log(`it comes`);
 	client.user.setActivity(`Bow. (wow)`);
 });
 
